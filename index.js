9ded0f123a6ef6d6e9f7d1879d2c5aa9387bf5a5
Tornike Borchkhadze <borchkhadzetornike@gmail.com>
2022-08-02 14:07:26 +0400

    add animations to buttons

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
         buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
         
     });
 
     document.addEventListener('keydown', function (event){
         makeSound(event.key);
+        buttonAnimation(event.key);
     });
 }
 
@@ -43,5 +45,13 @@ function makeSound(key){
         default:
             console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector(`.${currentKey}`);
+    activeButton.classList.add('pressed');
+    setTimeout(function(){
+        activeButton.classList.remove('pressed');
+    }, 100);
 }
 
